0148bbfe5b4cdee7cacc67370ba00f8c5d5b9e30
Deva <devadityabhatta@gmail.com>
2026-04-28 03:11:20 +0530

    fix gemini model path

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import google.generativeai as genai
 genai.configure(api_key="AIzaSyAX1RX6EU3F17h6N4IHLwBiwQMHjxcu0VA")
 
 # Use Gemini model
-model = genai.GenerativeModel("gemini-pro")
+model = genai.GenerativeModel("models/gemini-1.5-flash")
 
 app = FastAPI()
 
